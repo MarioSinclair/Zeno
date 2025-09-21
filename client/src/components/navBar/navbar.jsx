d36ebc16d75633accdf185bc88132154df2b00e2
MarioSinclair <mario.sinclairbz@gmail.com>
2025-09-21 12:40:05 -0400

    Fixed some layouts

--- client/src/components/navBar/navbar.jsx
+++ client/src/components/navBar/navbar.jsx
@@ -23,18 +23,6 @@ export default function Navbar() {
                     <button className="nav-button secondary-button">Log in</button>
                     <button className="nav-button primary-button">Sign up</button>
                     
-                    <div className="navbar-icons">
-                        <svg className="navbar-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                            <circle cx="12" cy="12" r="10"/>
-                            <path d="M2 12h20M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"/>
-                        </svg>
-                        <svg className="navbar-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                            <rect x="3" y="3" width="7" height="7"/>
-                            <rect x="14" y="3" width="7" height="7"/>
-                            <rect x="14" y="14" width="7" height="7"/>
-                            <rect x="3" y="14" width="7" height="7"/>
-                        </svg>
-                    </div>
                 </div>
             </div>
         </nav>
